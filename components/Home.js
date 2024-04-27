f89f251faf7bdcf38e5bf58143f25b8d4e806703
Béa <beatrice.becco@gmail.com>
2024-04-27 18:17:40 +0200

    with props and array moviesData

--- components/Home.js
+++ components/Home.js
@@ -1,23 +1,36 @@
 import styles from '../styles/Home.module.css';
+import 'antd/dist/antd.css';
 import Movie from './Movie';
 import { Button, Popover } from 'antd';
-import React from 'react';
+
 
 
 
 function Home() {
+  const moviesData = [
+    { title: 'Forrest Gump', poster: 'forrestgump.jpg', voteAverage: 9.2, voteCount: 22_705, overview: 'A man with a low IQ has accomplished great things in his life and been present during significant historic events—in each case.' },
+    { title: 'The Dark Knight', poster: 'thedarkknight.jpg', voteAverage: 8.5, voteCount: 27_547, overview: 'Batman raises the stakes in his war on crime and sets out to dismantle the remaining criminal organizations that plague the streets.' },
+    { title: 'Your name', poster: 'yourname.jpg', voteAverage: 8.5, voteCount: 8_691, overview: 'High schoolers Mitsuha and Taki are complete strangers living separate lives. But one night, they suddenly switch places.' },
+    { title: 'Iron Man', poster: 'ironman.jpg', voteAverage: 7.6, voteCount: 22_7726, overview: 'After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.' },
+    { title: 'Inception', poster: 'inception.jpg', voteAverage: 8.4, voteCount: 31_546, overview: 'Cobb, a skilled thief who commits corporate espionage by infiltrating the subconscious of his targets is offered a chance to regain his old life.' },
+  ];
+
+  const movies = moviesData.map(movie => {
+    return <Movie movieTitle={movie.title} poster={movie.poster} description={movie.overview} voteAverage={movie.voteAverage} vote={movie.voteCount} />
+  })
+
   const text = <span>Liked Movies</span>;
   const content = (
-    <div>
+    <div className={styles.popoverContent}>
       <p>Movie 1</p>
       <p>Movie 2</p>
     </div>
   )
-  
-  let movies = []
-  for (let i = 0; i < 10 ; i++){
-    movies.push(<Movie key={i}/>)
-  }
+
+  // let movies = []
+  // for (let i = 0; i < moviesData.length  ; i++){
+  //   movies.push(<Movie key={i}/>)
+  // }
   return (
     <div>
       <main className={styles.main}>
@@ -27,16 +40,16 @@ function Home() {
             <img className={styles.logoLetter} src="logoletter.png" alt="Letter Logo" />
           </div>
           <div>
-            <Popover placement="bottom" content={content} title={text} trigger="click">
-              <Button className={styles.likedMoviesBtn} type="primary">♥ 4 movie(s)</Button>
+            <Popover className={styles.popover} placement="bottom" content={content} title={text}>
+              <Button className={styles.likedMoviesBtn}>♥ 4 movie(s)</Button>
             </Popover>
           </div>
         </header>
-        <body>
-        <h1 className={styles.title}>LAST RELEASES</h1>
-        <div className={styles.moviesContainer}>
-        {movies}
-        </div>
+        <body className={styles.body}>
+          <h1 className={styles.title}>LAST RELEASES</h1>
+          <div className={styles.moviesContainer}>
+            {movies}
+          </div>
         </body>
       </main>
     </div>
--- components/Movie.js
+++ components/Movie.js
@@ -2,22 +2,29 @@ import styles from '../styles/Movie.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
 
-function Movie() {
+function Movie(props) {
+  let starStyle={}
   let stars = [];
   for (let i = 0; i < 10; i++) {
-    stars.push(<FontAwesomeIcon key={i} className={styles.star} icon={faStar} />)
+    if(i + 1 <= props.voteAverage){
+      stars.push(<FontAwesomeIcon key={i} className={styles.star} icon={faStar} style={starStyle ={'color':'#f1c40f'}} />)
+    }
+    else{
+      stars.push(<FontAwesomeIcon key={i} className={styles.star} icon={faStar}/>)
+    }    
   }
+  
   return (
     <div>
       <main className={styles.main}>
-        <img className={styles.poster} src='poster.jpg' alt='Poster' />
-        <h2 className={styles.movieTitle}>Movie Title</h2>
-        <h3 className={styles.description}>Description</h3>
+        <img className={styles.poster} src={props.poster} alt= {props.movieTitle} />
+        <h2 className={styles.movieTitle}>{props.movieTitle}</h2>
+        <h3 className={styles.description}>{props.description}</h3>
         <div className={styles.starsContainer}>
           {stars}
         </div>
         <p className={styles.vote}>
-          Nb de votes
+        {props.vote}(Nb de votes)
         </p>
       </main>
     </div>
