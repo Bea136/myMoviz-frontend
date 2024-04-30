d29853f6af76a78860bd0e9ae0ed94c31b9c6c4d
Béa <beatrice.becco@gmail.com>
2024-04-30 10:35:28 +0200

    Add personnalStars video like

--- components/Movie.js
+++ components/Movie.js
@@ -1,34 +1,73 @@
 import styles from '../styles/Movie.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar, faVideo, faHeart } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 function Movie(props) {
-  let starStyle={}
+  const [personalVote, setPersonalVote] = useState(0)
+  const [watchCount, setWatchCount] = useState(0)
+  const [liked, setLiked] = useState(false)
+
+//DISPLAY VOTE AVERAGE STARS
+  let starStyle = {}
   let stars = [];
   for (let i = 0; i < 10; i++) {
-    if(i + 1 <= props.voteAverage){
-      stars.push(<FontAwesomeIcon key={i} className={styles.star} icon={faStar} style={starStyle ={'color':'#f1c40f'}} />)
+    if (i + 1 <= props.voteAverage) {
+      stars.push(<FontAwesomeIcon key={i} className={styles.star} icon={faStar} style={starStyle = { 'color': '#f1c40f' }} />)
+    } else {
+      stars.push(<FontAwesomeIcon key={i} className={styles.star} icon={faStar} />)
     }
-    else{
-      stars.push(<FontAwesomeIcon key={i} className={styles.star} icon={faStar}/>)
-    }    
   }
-  
+  //DISPLAY PERSONNAL VOTE STARS
+  let personalStars = [];
+  let personalStarStyle = {}
+  for (let j = 0; j < 10; j++) {
+    const handleClickPersonalStar = () => {
+      setPersonalVote( j + 1)
+      console.log(personalVote);
+    }
+    if(personalVote >= j + 1){
+    personalStars.push(<FontAwesomeIcon onClick={handleClickPersonalStar} key={j} className={styles.personalStars} icon={faStar} style={personalStarStyle ={'color': 'blue'}} />)
+  } else {
+    personalStars.push(<FontAwesomeIcon onClick={handleClickPersonalStar} key={j} className={styles.personalStars} icon={faStar} />)
+  }
+}
+  //VIEWED FILMS
+  const handleClickCamera = () => {
+    setWatchCount(watchCount + 1);
+  }
+  let cameraStyle = {}
+  if (watchCount > 0) {
+    cameraStyle = { 'color': 'red' }
+  }
+  //LIKE OR UNLIKE FILMS
+  const handleClickLiked = () => {
+    setLiked(!liked)
+  }
+  let likeStyle = {}
+  if (liked) {
+    likeStyle = { 'color': 'red' }
+  }
+
+
   return (
     <div>
       <main className={styles.main}>
-        <img className={styles.poster} src={props.poster} alt= {props.movieTitle} />
+        <img className={styles.poster} src={props.poster} alt={props.movieTitle} />
         <h2 className={styles.movieTitle}>{props.movieTitle}</h2>
         <h3 className={styles.description}>{props.description}</h3>
-        <div className={styles.starsContainer}>
-          {stars}
+        <div className={styles.starsContainer}>{stars}</div>
+        <p className={styles.vote}>({props.vote})</p>
+        <div className={styles.personalStarsContainer}>
+          {personalStars}({personalVote})
         </div>
-        <p className={styles.vote}>
-        {props.vote}(Nb de votes)
-        </p>
+        <div className={styles.viewCount}>
+          <FontAwesomeIcon className={styles.video} onClick={handleClickCamera} icon={faVideo} style={cameraStyle} />
+          ({watchCount})
+        </div>
+        <FontAwesomeIcon className={styles.like} onClick={handleClickLiked} icon={faHeart} style={likeStyle} />
       </main>
     </div>
   );
 }
-
 export default Movie;
